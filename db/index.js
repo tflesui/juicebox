cecc044a1698fc0c0dab51901123615298c8d1a7
Francis Lesui <37851797+tflesui@users.noreply.github.com>
2021-05-08 20:36:47 -0700

    Added tags functionality

--- db/index.js
+++ db/index.js
@@ -73,6 +73,95 @@ const createPost = async ({
     }
 }
 
+const createTags = async tagList => {
+    if (tagList.length === 0) {
+        return;
+    }
+
+    const insertValues = tagList.map(
+        (_, index) => `$${index + 1}`).join('), (');
+    
+    const selectValues = tagList.map(
+        (_, index) => `$${index + 1}`).join(', ');
+
+    try {
+        await client.query(`
+            INSERT INTO tags(name)
+            VALUES (${ insertValues })    
+            ON CONFLICT (name) DO NOTHING;    
+        `, tagList);
+
+        const { rows } = await client.query(`
+            SELECT *
+            FROM tags
+            WHERE name
+            IN (${ selectValues });
+        `, tagList);
+
+        return rows;
+    } catch(err) {
+        throw err;
+    }
+}
+
+const createPostTag = async (postId, tagId) => {
+    try {
+        await client.query(`
+            INSERT INTO post_tags("postId", "tagId")
+            VALUES ($1, $2)
+            ON CONFLICT ("postId", "tagId") DO NOTHING;
+        `, [postId, tagId]);
+    } catch(err) {
+        throw err;
+    }
+}
+
+const addTagsToPost = async (postId, tagList) => {
+    try {
+        const createPostTagPromises = tagList.map(
+            tag => createPostTag(postId, tag.id)
+        );
+
+        await Promise.all(createPostTagPromises);
+
+        return await getPostById(postId);
+    } catch(err) {
+        throw err;
+    }
+}
+
+const getPostById = async postId => {
+    try {
+        const { rows: [ post ] } = await client.query(`
+            SELECT *
+            FROM posts
+            WHERE id=$1;
+        `, [postId]);
+
+        const { rows: tags } = await client.query(`
+            SELECT tags.*
+            FROM tags
+            JOIN post_tags ON tags.id=post_tags."tagId"
+            WHERE post_tags."postId"=$1;
+        `, [postId]);
+
+        const { rows: [ author ] } = await client.query(`
+            SELECT id, username, name, location
+            FROM users
+            WHERE id=$1;
+        `, [post.authorId]);
+
+        post.tags = tags;
+        post.author = author;
+
+        delete post.authorId;
+
+        return post;
+    } catch (err) {
+        throw err;
+    }
+}
+
 const updatePost = async (id, fields = {
     title,
     content,
@@ -161,7 +250,9 @@ module.exports = {
     updatePost,
     getUserById,
     getAllPosts,
-    getPostsByUser
+    getPostsByUser,
+    createTags,
+    addTagsToPost
 }
 
 
--- db/seed.js
+++ db/seed.js
@@ -7,7 +7,9 @@ const { client,
         getAllPosts,
         getPostsByUser,
         getUserById,
-        updatePost
+        updatePost,
+        createTags,
+        addTagsToPost
 } = require('./index');
 
 const createInitialUsers = async () => {
@@ -61,11 +63,36 @@ const createInitialPosts = async () => {
   }
 }
 
+const createInitialTags = async () => {
+  try {
+    console.log('Creating tags...');
+
+    const [happy, sad, inspo, catman] = await createTags([
+      '#happy',
+      '#worst-day-ever',
+      '#youcandoanything',
+      '#catmandoeverything'
+    ]);
+
+    const [postOne, postTwo, postThree] = await getAllPosts();
+
+    await addTagsToPost(postOne.id, [happy, inspo]);
+    await addTagsToPost(postTwo.id, [sad, inspo]);
+    await addTagsToPost(postThree.id, [happy, catman, inspo]);
+
+    console.log('Finished creating tags!');
+  } catch(err) {
+    throw err;
+  }
+}
+
 const dropTables = async () => {
   try{
       console.log('Dropping tables...');
 
       await client.query(`
+          DROP TABLE IF EXISTS post_tags;
+          DROP TABLE IF EXISTS tags;
           DROP TABLE IF EXISTS posts;
           DROP TABLE IF EXISTS users;
       `);
@@ -102,6 +129,21 @@ const createTables = async () => {
           );
       `);
 
+      await client.query(`
+            CREATE TABLE tags (
+              id SERIAL PRIMARY KEY,
+              name VARCHAR(255) UNIQUE NOT NULL
+            );
+      `);
+
+      await client.query(`
+            CREATE TABLE post_tags (
+              "postId" INTEGER REFERENCES posts(id),
+              "tagId" INTEGER REFERENCES tags(id),
+              UNIQUE ("postId", "tagId")
+            );
+      `);
+
       console.log('Finished building tables');
   } catch(err) {
       console.error('Error building tables!');
@@ -117,7 +159,9 @@ const rebuildDB = async () => {
       await createTables();
       await createInitialUsers();
       await createInitialPosts();
+      await createInitialTags();
   } catch(err) {
+    console.log('Error during rebuildDB');
       throw err;
   }
 }
