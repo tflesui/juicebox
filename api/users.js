9fae6af8cee7d04bcf822b5b46b7e9eb1df7b782
Francis Lesui <37851797+tflesui@users.noreply.github.com>
2021-05-15 18:40:51 -0700

    added user registration route

--- api/users.js
+++ api/users.js
@@ -3,6 +3,7 @@ const usersRouter = express.Router();
 const jwt = require('jsonwebtoken');
 const { getAllUsers } = require('../db');
 const { getUserByUsername } = require('../db');
+const { createUser } = require('../db');
 
 usersRouter.use((req, res, next) => {
     console.log('A request is being made to /users');
@@ -49,6 +50,42 @@ usersRouter.post('/login', async (req, res, next) => {
     } catch(error) {
       console.log(error);
       next(error);
+    }
+});
+
+usersRouter.post('/register', async (req, res, next) => {
+    const { username, password, name, location } = req.body;
+
+    try {
+        const _user = await getUserByUsername(username);
+
+        if (_user) {
+            next({
+                name: 'UserExistsError',
+                message: 'A user by that username already exists'
+            });
+        }
+
+        const user = await createUser({
+            username,
+            password,
+            name,
+            location,
+        });
+
+        const token = jwt.sign({
+            id: user.id,
+            username
+        }, process.env.JWT_SECRET, {
+            expiresIn: '1w'
+        });
+
+        res.send({
+            message: "Thank you for signing up",
+            token
+        });
+    } catch ({ name, message }) {
+        next({ name, message })
     }
 });
 
