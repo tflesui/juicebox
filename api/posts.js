53bf566c80ba54f1da8272fb862c5e8c744ebd07
Francis Lesui <37851797+tflesui@users.noreply.github.com>
2021-05-16 15:05:42 -0700

    Added filtering of inactive posts

--- api/posts.js
+++ api/posts.js
@@ -6,6 +6,21 @@ const { updatePost } = require('../db');
 const { getPostById } = require('../db');
 const { requireUser } = require('./utils');
 
+postsRouter.get('/', async (req, res) => {
+    try {
+        const allPosts = await getAllPosts();
+
+        const posts = allPosts.filter(post => {
+            return post.active || (req.user && post.author.id === req.user.id);
+        })
+
+        res.send({
+        posts
+        });
+    } catch ({ name, message }) {
+        next({ name, message });
+    }
+});
 
 postsRouter.post('/', requireUser, async (req, res, next) => {
     const { title, content, tags = "" } = req.body;
@@ -65,6 +80,28 @@ postsRouter.patch('/:postId', requireUser, async (req, res, next) => {
     }
   });
 
+  postsRouter.delete('/:postId', requireUser, async (req, res, next) => {
+      try {
+          const post = await getPostById(req.params.postId);
+
+          if (post && post.author.id === req.user.id) {
+              const updatedPost = await updatePost(post.id, { active: false });
+
+              res.send({ post: updatedPost });
+          } else {
+              next( post ? {
+                  name: "UnauthorizedUserError",
+                  message: "You cannot delete a post which is not yours"
+              } : {
+                  name: "PostNotFoundError",
+                  message: "That post does not exist"
+              });
+          }
+      } catch ({ name, message }) {
+          next({ name, message })
+      }
+  });
+
   postsRouter.use((req, res, next) => {
     console.log('A request is being made to /posts');
 
@@ -72,12 +109,5 @@ postsRouter.patch('/:postId', requireUser, async (req, res, next) => {
 });
 
 
-postsRouter.get('/', async (req, res) => {
-    const posts = await getAllPosts();
-
-    res.send({
-        posts
-    });
-});
 
 module.exports = postsRouter;
